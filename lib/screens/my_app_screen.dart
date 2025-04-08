687247698af109d87301b0a1161c1e1649ada796
nhat191024 <richberchannel01@gmail.com>
2025-04-08 14:06:28 +0700

    feat: implement MyApp widget with custom theme and routing

---
+++ lib/screens/my_app_screen.dart
@@ -0,0 +1,37 @@
+import 'package:learning_system/utils/imports.dart';
+
+class MyApp extends GetView<SplashController> {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShadApp.custom(
+      appBuilder:
+          (context, theme) => GetMaterialApp(
+            initialRoute: Pages.initialRoute,
+            getPages: Pages.routes,
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: false,
+              hintColor: CustomColors.secondaryText,
+              primaryColor: CustomColors.primary,
+              scaffoldBackgroundColor: CustomColors.white,
+              textTheme: const TextTheme(
+                displayLarge: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                displayMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                displaySmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                headlineMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                headlineSmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                titleLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
+                titleMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+                titleSmall: TextStyle(fontFamily: FontStyleTextStrings.medium),
+                bodySmall: TextStyle(fontSize: 10, fontFamily: FontStyleTextStrings.regular),
+                bodyLarge: TextStyle(fontSize: 13, fontFamily: FontStyleTextStrings.medium),
+                bodyMedium: TextStyle(fontSize: 13, fontFamily: FontStyleTextStrings.light),
+                labelLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
+              ),
+            ),
+          ),
+    );
+  }
+}
