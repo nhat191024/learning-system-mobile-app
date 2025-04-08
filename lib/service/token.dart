684507d031d06014a39bff5c32a1f7599765d7f6
nhat191024 <richberchannel01@gmail.com>
2025-04-08 18:27:26 +0700

    feat: add Token class for managing user authentication tokens

---
+++ lib/service/token.dart
@@ -0,0 +1,49 @@
+import 'package:learning_system/utils/imports.dart';
+
+class Token {
+  static dynamic getToken() async {
+    if (StorageService.checkData(key: LocalStorageKeys.token)) {
+      return StorageService.readData(key: LocalStorageKeys.token);
+    } else {
+      return null;
+    }
+  }
+
+  // static Future<bool> checkToken({bool isLogin = false}) async {
+  //   if (StorageService.checkData(key: LocalStorageKeys.token)) {
+  //     var url = Uri.parse("${Api.server}token-check");
+  //     var token = StorageService.readData(key: LocalStorageKeys.token);
+  //     try {
+  //       var response = await get(
+  //         url,
+  //         headers: {"Authorization": "Bearer $token"},
+  //       );
+  //       if (response.statusCode == 200) {
+  //         if (isLogin) Get.offAllNamed(Routes.homePage);
+  //         return true;
+  //       } else {
+  //         if (!isLogin) Get.offAllNamed(Routes.loginPage);
+  //         return false;
+  //       }
+  //     } catch (e) {
+  //       if (!isLogin) Get.offAllNamed(Routes.loginPage);
+  //       return false;
+  //     }
+  //   } else {
+  //     if (!isLogin) Get.offAllNamed(Routes.loginPage);
+  //     return false;
+  //   }
+  // }
+
+  static storeToken(String token) async {
+    if (StorageService.checkData(key: LocalStorageKeys.token)) {
+      StorageService.removeData(key: LocalStorageKeys.token);
+    }
+
+    StorageService.writeStringData(key: LocalStorageKeys.token, value: token);
+  }
+
+  static removeToken() {
+    StorageService.removeData(key: LocalStorageKeys.token);
+  }
+}
