d2d058fbed2be4b29dc87a8773c074ad61e8bade
nhat191024 <richberchannel01@gmail.com>
2025-04-08 16:32:59 +0700

    feat: refactor MyApp to use GetMaterialApp directly and enhance configuration

--- lib/screens/my_app_screen.dart
+++ lib/screens/my_app_screen.dart
@@ -5,33 +5,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShadApp.custom(
-      appBuilder:
-          (context, theme) => GetMaterialApp(
-            initialRoute: Pages.initialRoute,
-            getPages: Pages.routes,
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              useMaterial3: false,
-              hintColor: CustomColors.secondaryText,
-              primaryColor: CustomColors.primary,
-              scaffoldBackgroundColor: CustomColors.white,
-              textTheme: const TextTheme(
-                displayLarge: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                displayMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                displaySmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                headlineMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                headlineSmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                titleLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
-                titleMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
-                titleSmall: TextStyle(fontFamily: FontStyleTextStrings.medium),
-                bodySmall: TextStyle(fontSize: 10, fontFamily: FontStyleTextStrings.regular),
-                bodyLarge: TextStyle(fontSize: 13, fontFamily: FontStyleTextStrings.medium),
-                bodyMedium: TextStyle(fontSize: 13, fontFamily: FontStyleTextStrings.light),
-                labelLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
-              ),
-            ),
+    return GetMaterialApp(
+      initialRoute: Pages.initialRoute,
+      getPages: Pages.routes,
+      debugShowCheckedModeBanner: true,
+      defaultTransition: Transition.cupertino,
+      locale: const Locale('vi', 'VN'),
+      translations: Words(),
+      transitionDuration: const Duration(milliseconds: 500),
+      theme: ThemeData(
+        useMaterial3: false,
+        hintColor: CustomColors.secondaryText,
+        primaryColor: CustomColors.primary,
+        scaffoldBackgroundColor: CustomColors.white,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          displayMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          displaySmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          headlineMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          headlineSmall: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          titleLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
+          titleMedium: TextStyle(fontFamily: FontStyleTextStrings.regular),
+          titleSmall: TextStyle(fontFamily: FontStyleTextStrings.medium),
+          bodySmall: TextStyle(
+            fontSize: 10,
+            fontFamily: FontStyleTextStrings.regular,
           ),
+          bodyLarge: TextStyle(
+            fontSize: 13,
+            fontFamily: FontStyleTextStrings.medium,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 13,
+            fontFamily: FontStyleTextStrings.light,
+          ),
+          labelLarge: TextStyle(fontFamily: FontStyleTextStrings.medium),
+        ),
+      ),
     );
   }
 }
