9c4a801d9ce1e051cceefcd12b4d8bad038653a6
nhat191024 <richberchannel01@gmail.com>
2025-04-08 17:27:46 +0700

    feat: add AppColors class for consistent color management across the application

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:learning_system/utils/imports.dart';
+
+class AppColors {
+  // primary color
+  static const Color primary = Color(0xFF111111);
+  static const Color backgroundPrimary = Color(0xFFFFFFFF);
+
+  //Neutral color
+  static const Color primaryText = Color(0xFF111111);
+  static const Color primaryTextLight = Color(0xFFFFFFFF);
+  static const Color secondaryText = Color(0xFF646464);
+  static const Color disable = Color(0xFFA7A7A7);
+  static const Color border = Color(0xFFDEDEDE);
+  static const Color dividers = Color(0xFFEFEFEF);
+  static const Color background = Color(0xFFF6F6F6);
+  static const Color footer = Color(0xFF333333);
+  static const Color white = Color(0xFFFFFFFF);
+
+  //button color
+  static const Color buttonPrimary = Color(0xFF09090b);
+
+  //Functional color
+  static const Color errorMain = Color(0xFFE82C36);
+  static const Color errorLight = Color(0xFFFFF1F2);
+  static const Color infoMain = Color(0xFF1371DC);
+  static const Color infoLight = Color(0xFFEEF9FF);
+  static const Color successMain = Color(0xFF54B12A);
+  static const Color successLight = Color(0xFFF1FBEA);
+}
