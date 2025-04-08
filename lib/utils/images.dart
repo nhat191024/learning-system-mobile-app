46784f4f06cc61bce023a4d6e7f88e3bbf6b5c0f
nhat191024 <richberchannel01@gmail.com>
2025-04-08 17:27:50 +0700

    feat: add Images class for managing image asset paths in the application

---
+++ lib/utils/images.dart
@@ -0,0 +1,8 @@
+class Images {
+  static const String login = 'assets/images/login.png';
+
+  //icons
+  static const String searchIcon = 'assets/icons/search.svg';
+  static const String penIcon = 'assets/icons/pen.svg';
+  static const String googleIcon = 'assets/icons/google.svg';
+}
