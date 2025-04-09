4abbb09c9210316d0d16856160359d42e93087c9
nhat191024 <richberchannel01@gmail.com>
2025-04-09 20:53:32 +0700

    fix: update API server URL and test token for local development

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,6 @@
 class Api {
-  static const server = 'https://127.0.0.1/api/';
+  static const server = 'http://192.168.1.4:8000/api/';
   static const apiTimeOut = 10;
   //this shit only work local lol
-  static const testToken = "3|3wCvYZw6uV7m92dvn15IZbWZ6WdwSg9gdm3PxX1ded9e41d9";
+  static const testToken = "enrollments1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
 }
