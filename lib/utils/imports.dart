b4050bbb69672eb947de18a7b4cdf5b3f9da0a2c
nhat191024 <richberchannel01@gmail.com>
2025-04-09 20:53:44 +0700

    feat: add Bottom Navigation and its controller and binding imports

--- lib/utils/imports.dart
+++ lib/utils/imports.dart
@@ -6,15 +6,18 @@ export 'package:flutter/material.dart';
 
 //screen imports
 export 'package:learning_system/screens/login_screen.dart';
+export 'package:learning_system/screens/bottom_nav.dart';
 export 'package:learning_system/screens/class_list_screen.dart';
 
 //controller imports
 export 'package:learning_system/controllers/login_controller.dart';
 export 'package:learning_system/controllers/class_controller.dart';
+export 'package:learning_system/controllers/bottom_nav_controller.dart';
 
 //binding imports
 export 'package:learning_system/bindings/login_binding.dart';
 export 'package:learning_system/bindings/class_binding.dart';
+export 'package:learning_system/bindings/bottom_nav_binding.dart';
 
 //utils imports
 export 'package:learning_system/routes/pages.dart';
