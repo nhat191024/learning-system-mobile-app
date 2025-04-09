9f29d9d8325d5dc6f3d8510b5c6d80a662794c0c
nhat191024 <richberchannel01@gmail.com>
2025-04-09 21:33:50 +0700

    fix: update testToken value for correct API authentication

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,5 +2,5 @@ class Api {
   static const server = 'http://192.168.1.4:8000/api/';
   static const apiTimeOut = 10;
   //this shit only work local lol
-  static const testToken = "enrollments1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
+  static const testToken = "1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
 }
