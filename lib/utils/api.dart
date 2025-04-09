8f0162f4603d79aa8a5ddbb7846272bbb1bebe80
nhat191024 <richberchannel01@gmail.com>
2025-04-09 23:55:13 +0700

    fix: update API server URL for production environment

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const server = 'http://192.168.1.4:8000/api/';
+  static const server = 'https://nhat-pc.tail57cba4.ts.net/api/';
   static const apiTimeOut = 10;
   //this shit only work local lol
   static const testToken = "1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
