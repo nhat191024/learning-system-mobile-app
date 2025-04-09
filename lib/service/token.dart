2509850c80a495cc842838fe313b1e281f71db8d
nhat191024 <richberchannel01@gmail.com>
2025-04-09 21:55:34 +0700

    refactor: simplify checkToken method for improved readability and flow

--- lib/service/token.dart
+++ lib/service/token.dart
@@ -9,32 +9,29 @@ class Token {
     }
   }
 
-  //TODO: gonna fix it later lmao
-  // static Future<bool> checkToken({bool isLogin = false}) async {
-  //   if (StorageService.checkData(key: LocalStorageKeys.token)) {
-  //     var url = Uri.parse("${Api.server}token-check");
-  //     var token = StorageService.readData(key: LocalStorageKeys.token);
-  //     try {
-  //       var response = await get(
-  //         url,
-  //         headers: {"Authorization": "Bearer $token"},
-  //       );
-  //       if (response.statusCode == 200) {
-  //         if (isLogin) Get.offAllNamed(Routes.homePage);
-  //         return true;
-  //       } else {
-  //         if (!isLogin) Get.offAllNamed(Routes.loginPage);
-  //         return false;
-  //       }
-  //     } catch (e) {
-  //       if (!isLogin) Get.offAllNamed(Routes.loginPage);
-  //       return false;
-  //     }
-  //   } else {
-  //     if (!isLogin) Get.offAllNamed(Routes.loginPage);
-  //     return false;
-  //   }
-  // }
+  static Future<bool> checkToken() async {
+    if (StorageService.checkData(key: LocalStorageKeys.token)) {
+      var url = Uri.parse("${Api.server}token-check");
+      var token = StorageService.readData(key: LocalStorageKeys.token);
+      try {
+        var response = await get(
+          url,
+          headers: {"Authorization": "Bearer $token"},
+        );
+
+        if (response.statusCode == 200) {
+          Get.offAllNamed(Routes.bottomNav);
+          return true;
+        }
+
+        return false;
+      } catch (e) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
 
   static storeToken(String token) async {
     if (StorageService.checkData(key: LocalStorageKeys.token)) {
