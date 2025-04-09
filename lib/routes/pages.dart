ef5a2efc6a800260fe2e09adad3b9105f282bf10
nhat191024 <richberchannel01@gmail.com>
2025-04-09 20:53:56 +0700

    feat: update initial route to Bottom Navigation and add route constants

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -5,13 +5,25 @@ part 'routes.dart';
 class Pages {
   Pages._();
 
-  static const initialRoute = Routes.loginScreen;
+  static const initialRoute = Routes.bottomNav;
 
   static final routes = [
     GetPage(
       name: Routes.loginScreen,
       page: () => const LoginScreen(),
       binding: LoginBinding(),
+    ),
+
+    GetPage(
+      name: Routes.bottomNav,
+      page: () => const BottomNav(),
+      binding: BottomNavBinding(),
+    ),
+
+    GetPage(
+      name: Routes.classListScreen,
+      page: () => ClassListScreen(),
+      binding: ClassBinding(),
     ),
   ];
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,8 +4,12 @@ abstract class Routes {
   Routes._();
 
   static const loginScreen = _Paths.loginScreen;
+  static const bottomNav = _Paths.bottomNav;
+  static const classListScreen = _Paths.classListScreen;
 }
 
 abstract class _Paths {
   static const loginScreen = '/login-screen';
+  static const bottomNav = '/bottom-nav';
+  static const classListScreen = '/class-list-screen';
 }
