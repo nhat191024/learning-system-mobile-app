1a38788838ef43531aca3f0a69eb4f781d14183b
nhat191024 <richberchannel01@gmail.com>
2025-04-09 21:34:31 +0700

    feat: add ClassController and CourseController dependencies in BottomNavBinding

--- lib/bindings/bottom_nav_binding.dart
+++ lib/bindings/bottom_nav_binding.dart
@@ -4,5 +4,7 @@ class BottomNavBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<BottomNavController>(() => BottomNavController());
+    Get.lazyPut<ClassController>(() => ClassController());
+    Get.lazyPut<CourseController>(() => CourseController());
   }
 }
