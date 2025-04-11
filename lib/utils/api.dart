420e0842c339117e0e36b5199ab94e93c29cc850
nhat191024 <richberchannel01@gmail.com>
2025-04-11 17:32:47 +0700

    fix: update API server URL to correct endpoint

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const server = 'https://nhat-pc.tail57cba4.ts.net/api/';
+  static const server = 'https://nhat-pc.tail67a81b.ts.net/api/';
   static const apiTimeOut = 10;
   //this shit only work local lol
   static const testToken = "1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
