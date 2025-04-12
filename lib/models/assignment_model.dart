00df86b4ad9d8c34e0cab44bc0fd29a118a5b539
nhat191024 <richberchannel01@gmail.com>
2025-04-12 23:04:04 +0700

    feat: add AssignmentModel for assignment data handling

---
+++ lib/models/assignment_model.dart
@@ -0,0 +1,49 @@
+class AssignmentModel {
+  int? id;
+  String? type;
+  String? title;
+  String? description;
+  String? duration;
+  String? startDate;
+  String? dueDate;
+  String? status;
+  bool? isSubmitted;
+
+  AssignmentModel({
+    required this.id,
+    required this.type,
+    required this.title,
+    required this.description,
+    required this.duration,
+    required this.startDate,
+    required this.dueDate,
+    required this.status,
+    required this.isSubmitted,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'type': type,
+      'title': title,
+      'description': description,
+      'duration': duration,
+      'startDate': startDate,
+      'dueDate': dueDate,
+      'status': status,
+      'isSubmitted': isSubmitted,
+    };
+  }
+
+  AssignmentModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    type = map['type'];
+    title = map['title'];
+    description = map['description'];
+    duration = map['duration'];
+    startDate = map['startDate'];
+    dueDate = map['dueDate'];
+    status = map['status'];
+    isSubmitted = map['isSubmitted'];
+  }
+}
