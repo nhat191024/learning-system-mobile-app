da246df0056c494e71dd3903b6540111e04c4c6d
nhat191024 <richberchannel01@gmail.com>
2025-04-12 23:10:53 +0700

    feat: add assignment screen import to class detail screen

--- lib/utils/imports.dart
+++ lib/utils/imports.dart
@@ -10,6 +10,7 @@ export 'package:learning_system/screens/bottom_nav.dart';
 export 'package:learning_system/screens/class_list_screen.dart';
 export 'package:learning_system/screens/class_detail_screen.dart';
 export 'package:learning_system/screens/class_detail/overview_screen.dart';
+export 'package:learning_system/screens/class_detail/assignment_screen.dart';
 export 'package:learning_system/screens/course_list_screen.dart';
 export 'package:learning_system/screens/profile_screen.dart';
 
