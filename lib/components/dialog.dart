f5ae07197404ce9bf29cfe19ba16aec50d122913
nhat191024 <richberchannel01@gmail.com>
2025-04-13 22:49:31 +0700

    feat: refactor NotificationDialog and add custom button functionality

--- lib/components/dialog.dart
+++ lib/components/dialog.dart
@@ -4,11 +4,7 @@ class NotificationDialog extends StatelessWidget {
   final String title;
   final String message;
 
-  const NotificationDialog({
-    super.key,
-    required this.title,
-    required this.message,
-  });
+  const NotificationDialog({super.key, required this.title, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -58,15 +54,96 @@ class NotificationDialog extends StatelessWidget {
   }
 }
 
+class NotificationDialogWithCustomButton extends StatelessWidget {
+  final String title;
+  final String message;
+  final String btnText;
+  final Color btnTextColor;
+  final Color btnColor;
+  final VoidCallback onTap;
+
+  const NotificationDialogWithCustomButton({
+    super.key,
+    required this.title,
+    required this.message,
+    required this.btnText,
+    this.btnTextColor = AppColors.primaryTextLight,
+    this.btnColor = AppColors.primary,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                color: AppColors.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 14,
+                color: AppColors.primaryText,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomButton(
+                  onTap: () {
+                    Get.back();
+                  },
+                  btnText: 'close'.tr,
+                  width: 100,
+                  height: 40,
+                  textSize: 14,
+                  textColor: AppColors.primaryTextLight,
+                  fontFamily: FontStyleTextStrings.medium,
+                  btnColor: AppColors.primary,
+                  borderColor: AppColors.primary,
+                  borderRadius: 5,
+                ),
+                const SizedBox(width: 10),
+                CustomButton(
+                  onTap: onTap,
+                  btnText: btnText,
+                  width: 100,
+                  height: 40,
+                  textSize: 14,
+                  textColor: btnTextColor,
+                  fontFamily: FontStyleTextStrings.medium,
+                  btnColor: btnColor,
+                  borderColor: AppColors.primary,
+                  borderRadius: 5,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class NotificationDialogWithoutButton extends StatelessWidget {
   final String title;
   final String message;
 
-  const NotificationDialogWithoutButton({
-    super.key,
-    required this.title,
-    required this.message,
-  });
+  const NotificationDialogWithoutButton({super.key, required this.title, required this.message});
 
   @override
   Widget build(BuildContext context) {
