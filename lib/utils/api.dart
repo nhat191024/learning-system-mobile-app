a070070ec7e46b9c4bd2cb8e4c03d1e25dd7e9aa
nhat191024 <richberchannel01@gmail.com>
2025-04-17 17:52:29 +0700

    fix: correct IP address format in testServer URL

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,6 @@
 class Api {
   static const server = 'https://learning-system-web.taiyo.space/api/';
-  static const testServer = 'http://192/168.1.4:8000/api/';
+  static const testServer = 'http://192.168.1.4:8000/api/';
   static const apiTimeOut = 10;
   //this shit only work local lol
   static const testToken = "1|7YTUzjNOEr9hgQcd9APf77pNK6NJLc5m2uEPdC17319231aa";
